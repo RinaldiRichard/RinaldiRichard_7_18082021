51558908e180e1f416a540a180d985a815e01bf6
RinaldiRichard <81157374+RinaldiRichard@users.noreply.github.com>
2021-08-30 09:36:24 +0200

    Ajout btn suppr et modif style

--- components/Comment.js
+++ components/Comment.js
@@ -32,16 +32,16 @@ export default function Form() {
 
   return (
     <div className=" px-4 col-12 col-sm-10 col-lg-6 w-100 mh-50">
-      <form onSubmit={addTodo} className="mb-3">
-        <label htmlFor="todo" className="form-label mt-3">
-          Dites quelque chose : 
+      <form onSubmit={addTodo} className="mb-3 d-flex justify-content-between">
+        <label htmlFor="commentaire" className="form-label mt-3"> 
         </label>
         <input
+         placeholder="Ecrivez un commentaire"
           value={stateInput}
           onInput={(e) => linkedInput(e.target.value)}
           type="text"
-          className="form-control"
-          id="todo"
+          className="form-control w-auto"
+          id="commentaire"
         />
         <button className="mt-2 btn btn-primary d-block">Envoyer</button>
       </form>
--- components/Item.js
+++ components/Item.js
@@ -1,9 +1,12 @@
-
-
-export default function Item() {
-    return (
-        <div>
-            <h1>hello</h1>
-        </div>
-    )
+export default function Item(props) {
+  return (
+    <li className="border justify-content-between align-item-center d-flex m-1">
+      <div className="p-2">{props.txt} </div>
+      <button
+        className="btn btn-danger h-20"
+        onClick={() => props.delFunc(props.id)}>
+        Supprimer
+      </button>
+    </li>
+  );
 }
