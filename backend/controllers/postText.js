7ab1078559009a717abacc549f9e5b148439e9a6
RinaldiRichard <81157374+RinaldiRichard@users.noreply.github.com>
2021-10-14 17:05:31 +0200

    Delete backend directory

--- backend/controllers/postText.js
+++
@@ -1,9 +0,0 @@
-const { PostsText } = require("../models");
-
-exports.create = async (req, res) => {
-  const post = req.body;
-  post.username = req.user.username;
-  post.UserId = req.user.id
-  await PostsText.create(post);
-  res.json(post);
-};
